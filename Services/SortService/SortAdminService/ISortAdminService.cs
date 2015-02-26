ea6116b5a0b568e804c43c08ce84840dd336eab5
mkoren <mkoren@pgs-soft.com>
2015-02-26 11:28:42 +0100

    lacking file

---
+++ Services/SortService/SortAdminService/ISortAdminService.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using Models.ViewModels;
+
+namespace Services.SortService.SortAdminService
+{
+    public interface ISortAdminService
+    {
+        IEnumerable<AdminAdvertToShow> SortAdverts(IEnumerable<AdminAdvertToShow> advertsToShow, AdminSortOption? adminSortOption, bool sortDescAsc);
+    }
+}
