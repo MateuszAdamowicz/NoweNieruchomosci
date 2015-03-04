a6ae76b6abaf88ceba06ba9dae44f451df9df68c
mkoren <mkoren@pgs-soft.com>
2015-03-04 12:52:36 +0100

    added missing files

---
+++ Models/ViewModels/AdminIndexFilterOptions.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Models.ViewModels
+{
+    public class AdminIndexFilterOptions
+    {
+        public List<SelectOption> AdTypes { get; set; }
+        public List<SelectOption> ToLet { get; set; }
+        public List<SelectOption> PerPage { get; set; }
+        public List<SelectOption> Cities { get; set; }
+    }
+}
